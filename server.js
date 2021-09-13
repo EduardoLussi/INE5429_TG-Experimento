f4863a3359bfd24ded702510e2b3ba6d5f9212a8
Eduardo Lussi <=>
2021-09-13 19:20:09 -0300

    port

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.get('/', (req, res) => {
     res.send("<h1>Let's Encrypt!</h1>");
 });
 
-app.listen(8080);
+app.listen(PORT);
