56ceaff4c5a6fe75225059cbb2f0d2ffc6242e5c
EduardoLussi <eduardolussi@gmail.com>
2021-09-17 11:12:42 -0300

    cerbot

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send("<h1>Let's Encrypt!</h1>");
+app.get('/.well-known/acme-challenge/EUsjOqnzj0dB1hg9QR-nQrm471FSXj0NG9VBusLjFc4', (req, res) => {
+    res.send("EUsjOqnzj0dB1hg9QR-nQrm471FSXj0NG9VBusLjFc4.JnToXCNdZmBk7IIFq88_ZK3aI2_2bb5Iz4LXMSzE6WA");
 });
+
+app.get('/', (req, res) => {
+    res.send("<h1>Let's Encrypt</h1>");
+})
 
 app.listen(PORT);
