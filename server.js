6160449e5e9f737c0dde3c194e5f17e4ebb276f8
EduardoLussi <eduardolussi@gmail.com>
2021-09-18 15:22:34 -0300

    manualcertificate

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
+
 const app = express();
 
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
@@ -7,6 +11,26 @@ app.get('/.well-known/acme-challenge/evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0
 	res.send('evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0.JnToXCNdZmBk7IIFq88_ZK3aI2_2bb5Iz4LXMSzE6WA');
 });
 
-app.listen(80, () => {
-  console.log('HTTP server running on port 80');
+app.get('/', (req, res) => {
+	res.send("<h1>Let's Encrypt!</h1>");
+})
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/letsencrypt.onrender.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/letsencrypt.onrender.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/letsencrypt.onrender.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
