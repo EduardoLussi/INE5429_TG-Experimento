27d1121685cd1ffad77835a2d6399c6aefe86e7f
EduardoLussi <eduardolussi@gmail.com>
2021-09-18 08:43:04 -0300

    getonrender

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-app.get('/.well-known/acme-challenge/EUsjOqnzj0dB1hg9QR-nQrm471FSXj0NG9VBusLjFc4', (req, res) => {
-    res.send("EUsjOqnzj0dB1hg9QR-nQrm471FSXj0NG9VBusLjFc4.JnToXCNdZmBk7IIFq88_ZK3aI2_2bb5Iz4LXMSzE6WA");
+app.get('/.well-known/acme-challenge/evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0', (req, res) => {
+	res.send('evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0.JnToXCNdZmBk7IIFq88_ZK3aI2_2bb5Iz4LXMSzE6WA');
 });
 
-app.get('/', (req, res) => {
-    res.send("<h1>Let's Encrypt</h1>");
-})
-
-app.listen(PORT);
+app.listen(80, () => {
+  console.log('HTTP server running on port 80');
+});
