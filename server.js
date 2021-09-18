bed540673a0947665eb3082a8cab9d95f258c886
EduardoLussi <eduardolussi@gmail.com>
2021-09-18 15:50:54 -0300

    validate

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.get('/.well-known/acme-challenge/evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0
 	res.send('evUiu6WMCLURXX7Ev72nRSMQWtTSkNm98pJ-qFaOHu0.JnToXCNdZmBk7IIFq88_ZK3aI2_2bb5Iz4LXMSzE6WA');
 });
 
+app.listen(80);
+/*
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
@@ -31,4 +33,4 @@ httpServer.listen(80, () => {
 const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
-});
+});*/
